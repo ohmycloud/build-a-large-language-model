f54354ded7ffc18993b6f0a2c55bccd659e1fe14
ohmycloud <ohmycloudy@gmail.com>
2025-01-08 18:38:38 +0800

    dataloader

---
+++ ch02-working-with-text-data/data_loader.py
@@ -0,0 +1,11 @@
+from sliding_sampling import GPTDatasetV1, create_dataloader_v1
+
+with open('the-verdict.txt', 'r', encoding='utf-8') as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+second_batch = next(data_iter)
+print(second_batch)
